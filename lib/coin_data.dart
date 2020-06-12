3fc4439202167f9a5a6082c47d46886bd7b0bdef
zorer <ahmetcj4@hotmail.com>
2020-06-12 15:17:22 +0300

    just finished crypto coin challenge

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,25 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '0FC6E356-9DE1-secret-key-E84E936D0C3C';
+
+class CoinData {
+  Future<dynamic> getCoinData(String crypto, String quote) async {
+    try {
+      final response = await get(
+        '$coinAPIURL/$crypto/$quote',
+        headers: {'X-CoinAPI-Key': apiKey},
+      );
+
+      if (response.statusCode == 200) {
+        print(response.body);
+        return jsonDecode(response.body);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +11,46 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String quote = 'USD';
+  final cryptoRateList = cryptoList.map((e) => '?').toList();
+
+  CupertinoPicker iosPicker() => CupertinoPicker(
+        itemExtent: 32,
+        onSelectedItemChanged: (value) =>
+            setState(() => setQuote(currenciesList[value])),
+        children: currenciesList.map((e) => Text(e)).toList(),
+      );
+
+  DropdownButton<String> androidDropdown() => DropdownButton(
+        value: quote,
+        items: currenciesList
+            .map((e) => DropdownMenuItem(child: Text(e), value: e))
+            .toList(),
+        onChanged: (value) => setQuote(value),
+      );
+
+  void setQuote(String quote) {
+    this.quote = quote;
+    getData();
+  }
+
+  void getData() {
+    final coinData = CoinData();
+    cryptoList.asMap().entries.forEach((crypto) async {
+      var data = await coinData.getCoinData(crypto.value, quote);
+      if (data != null) {
+        setState(() => cryptoRateList[crypto.key] =
+            '${(double.tryParse(data['rate'].toString()) ?? 0.0).round()}');
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,33 +61,42 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: cryptoList
+                .asMap()
+                .entries
+                .map((crypto) => Padding(
+                      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                      child: Card(
+                        color: Colors.lightBlueAccent,
+                        elevation: 5.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 15.0, horizontal: 28.0),
+                          child: Text(
+                            '1 ${crypto.value} = ${cryptoRateList[crypto.key]} $quote',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ))
+                .toList(),
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? iosPicker() : androidDropdown(),
           ),
         ],
       ),
